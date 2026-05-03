33c98edeedb7a6fb798647df0ff07a559979eec6
Vitaliy Garnashevich <vgarnashevich@gmail.com>
2026-05-03 20:13:52 +0200

    Simplify exception handling

--- src/main/java/com/github/vsg/kyotu/temperature/api/exception/ApiExceptionHandler.java
+++ src/main/java/com/github/vsg/kyotu/temperature/api/exception/ApiExceptionHandler.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -18,24 +17,14 @@ public class ApiExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(ApiExceptionHandler.class);
     
-    @ExceptionHandler(CityNotFoundException.class)
-    public ProblemDetail handleCityNotFound(CityNotFoundException ex) {
+    @ExceptionHandler({CityNotFoundException.class, NoResourceFoundException.class})
+    public ProblemDetail handleNotFound(Exception ex) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
     }
     
-    @ExceptionHandler(DataNotAvailableException.class)
-    public ProblemDetail handleDataNotAvailable(DataNotAvailableException ex) {
+    @ExceptionHandler({DataNotAvailableException.class, InvalidDataFormatException.class})
+    public ProblemDetail handleUnavailable(Exception ex) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.SERVICE_UNAVAILABLE, "Temporarily unavailable");
-    }
-
-    @ExceptionHandler(InvalidDataFormatException.class)
-    public ProblemDetail handleInvalidDataFormat(InvalidDataFormatException ex) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.SERVICE_UNAVAILABLE, "Temporarily unavailable");
-    }
-    
-    @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<Void> handleNotFound(NoResourceFoundException ex) {
-        return ResponseEntity.notFound().build();
     }
     
     @ExceptionHandler(Exception.class)
