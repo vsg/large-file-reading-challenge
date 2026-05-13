6985d899cf522c7352bb266e3bf2fad6bc9918ca
Vitaliy Garnashevich <vgarnashevich@gmail.com>
2026-05-13 08:31:35 +0200

    Fix formatting

--- src/main/java/com/github/vsg/kyotu/temperature/TemperatureApplication.java
+++ src/main/java/com/github/vsg/kyotu/temperature/TemperatureApplication.java
@@ -8,8 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class TemperatureApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TemperatureApplication.class, args);
-	}
-	
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureApplication.class, args);
+    }
+    
 }
